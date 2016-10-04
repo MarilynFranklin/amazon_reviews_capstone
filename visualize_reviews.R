91b8cce93097fb8bac33defbb85b38ee27932d4d
Marilyn <marilyn.j.franklin@gmail.com>
2016-10-04 17:34:37 -0500

    Visualize review sentiment

---
+++ visualize_reviews.R
@@ -0,0 +1,66 @@
+library(reshape2)
+library(plyr)
+library(ggplot2)
+library(dplyr)
+library(data.table)
+library(tm)
+library(qdap)
+
+qdap_clean <- function(x) {
+  x <- replace_abbreviation(x)
+  x <- replace_contraction(x)
+  x <- replace_ordinal(x)
+  x <- replace_symbol(x)
+  x <- tolower(x)
+  return(x)
+}
+
+reviews <- fread("ScoredReviews.csv", header = TRUE, stringsAsFactors = FALSE)
+
+reviews_with_sentiment <- reviews %>% select(anger, anticipation, disgust, fear,
+                                             joy, sadness, surprise, trust,
+                                             negative, positive, is_helpful, Id)
+melted <- melt(reviews_with_sentiment, id.vars=c("Id", "is_helpful"))
+means <- ddply(melted, c("is_helpful", "variable"), summarise, mean=mean(value))
+
+ggplot(data = means, aes(x = factor(is_helpful), y = mean)) +
+  geom_bar(stat = "identity", position = "dodge", aes(fill = variable))
+
+helpful_reviews <- reviews %>% filter(is_helpful == 1)
+unhelpful_reviews <- reviews %>% filter(is_helpful == 0)
+
+# Clean text
+helpful   <- qdap_clean(helpful_reviews$Text)
+unhelpful <- qdap_clean(unhelpful_reviews$Text)
+
+my_stopwords <- c(stopwords('english'))
+
+helpful.corpus <- Corpus(VectorSource(helpful))
+helpful.corpus <- tm_map(helpful.corpus, removePunctuation)
+helpful.corpus <- tm_map(helpful.corpus, removeWords, my_stopwords)
+helpful.dtm <- TermDocumentMatrix(helpful.corpus)
+helpful.dtm2 <- removeSparseTerms(helpful.dtm, sparse=0.95)
+helpful.df <- as.data.frame(inspect(helpful.dtm2))
+helpful.df.scale <- scale(helpful.df)
+helpful_d <- dist(helpful.df.scale, method = "euclidean")
+helpful_fit <- hclust(helpful_d, method="ward.D")
+
+unhelpful.corpus <- Corpus(VectorSource(unhelpful))
+unhelpful.corpus <- tm_map(unhelpful.corpus, removePunctuation)
+unhelpful.corpus <- tm_map(unhelpful.corpus, removeWords, my_stopwords)
+unhelpful.dtm <- TermDocumentMatrix(unhelpful.corpus)
+unhelpful.dtm2 <- removeSparseTerms(unhelpful.dtm, sparse=0.95)
+unhelpful.df <- as.data.frame(inspect(unhelpful.dtm2))
+unhelpful.df.scale <- scale(unhelpful.df)
+unhelpful_d <- dist(unhelpful.df.scale, method = "euclidean")
+unhelpful_fit <- hclust(unhelpful_d, method="ward.D")
+
+
+# plot reviews
+plot(unhelpful_fit)
+groups <- cutree(unhelpful_fit, k=5)
+rect.hclust(unhelpful_fit, k=5, border="red")
+
+plot(helpful_fit)
+groups <- cutree(helpful_fit, k=5)
+rect.hclust(helpful_fit, k=5, border="red")
